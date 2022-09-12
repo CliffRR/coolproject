f235070f7fc44e7d15a5a43f72943f982c7a4732
CliffRR <cliff.reinoso98@yahoo.com>
2022-09-11 22:00:40 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// //console.log(process.argv)
+// const foo = process.argv[2]
+// console.log(foo)
+
+// //console.log(process.env)
+
+// const user = process.env.USERNAME
+// console.log(user)
+
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res) => {
+    res.json('hello, there')
+})
+
+const port = process.env.PORT || 9000
+
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
